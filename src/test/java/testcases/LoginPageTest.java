a7c05058530b521b318df3e981f67841198013d8
ADMIN <ADMIN@DESKTOP-C1M01HP>
2023-03-03 11:58:01 +0530

    My test changes in the project

--- src/test/java/testcases/LoginPageTest.java
+++ src/test/java/testcases/LoginPageTest.java
@@ -29,10 +29,7 @@ public class LoginPageTest extends TestBase {
 
 	public void LoginPageGetTitle() throws InterruptedException {
 
-		loginPage.homepageLogin(prop.getProperty("username"),prop.getProperty("password"));
-		String actualRes = loginPage.validateLoginPageTitle();
-//added lines
-		assertEquals(actualRes, "OrangeHRM");
+		System.out.println("Test commit in framework");
 	}
 
 	@AfterMethod()
